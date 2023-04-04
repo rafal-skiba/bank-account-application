7ff619ae548672ea65e1e5cfb4fd338971b68718
Rafał Skiba <rr.skiba92@gmail.com>
2023-04-04 23:30:07 +0200

    current date show  added

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1234,
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-07-26T17:01:17.194Z",
+    "2020-07-28T23:36:17.929Z",
+    "2020-08-01T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT"
 };
 
 const account2 = {
@@ -17,6 +29,19 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 const account3 = {
@@ -24,6 +49,18 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 const account4 = {
@@ -31,6 +68,18 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -67,9 +116,9 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /// function forEach reading values from account1
 
-const displayMovements = function(movements, sort = false) {
+const displayMovements = function(acc, sort = false) {
 
-const movs = sort ? movements.slice().sort((a,b) => a -b) : movements;
+const movs = sort ? acc.movements.slice().sort((a,b) => a -b) : acc.movements;
 
 containerMovements.innerHTML = '';
 
@@ -160,9 +209,9 @@ const totalDepo = movements.filter(depo => depo > 0).map(depo => depo * eurotoUs
 let currentAccount;
 
 const updateUI = (acc) => {
-  displayMovements(acc.movements);
-calcPrintBalance(acc);
-calcDisplaySummary(acc)
+  displayMovements(acc);
+  calcPrintBalance(acc);
+  calcDisplaySummary(acc)
 }
 
 
@@ -260,7 +309,10 @@ stateOfSorted = !stateOfSorted;
 
 })
 
+const now = new Date();
+
+const day =`${now.getDate()}`.padStart(2,0);
+const month =`${now.getMonth()+1}`.padStart(2,0);
 
 
-
-
+labelDate.textContent = `${day}/${month}/${new Date().getFullYear()}`
